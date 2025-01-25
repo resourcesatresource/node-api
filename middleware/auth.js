20704b7cf359703de41f09ae32121735e837b6de
Saurav Sanjay <resourcesatresource@gmail.com>
2025-01-25 22:43:05 +0530

    [Fixed]: Use the latest user config from the DB in `auth` middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,16 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-function auth(req, res, next) {
+const { getByEmail } = require("../models/user");
+
+async function auth(req, res, next) {
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Not authorized");
 
   try {
     const decoded = jwt.verify(token, config.get("jwtprivatekey"));
-    req.user = decoded;
+    const user = await getByEmail(decoded.email);
+    req.user = user;
     next();
   } catch (error) {
     res.status(400).send("Invalid token");
