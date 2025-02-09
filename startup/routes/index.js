142e06eb900834d0b789cbcb4fd0b6c6246821b2
Saurav Sanjay <resourcesatresource@gmail.com>
2025-02-09 17:41:32 +0530

    [Added-#15]: Route for health check

--- startup/routes/index.js
+++ startup/routes/index.js
@@ -2,9 +2,16 @@ const express = require("express");
 
 const api = require("./api");
 const { error, logger } = require("../../middleware");
+const { CUSTOM_RESPONSE_STATUS } = require("../../constants");
 
 module.exports = function (app) {
   app.use(express.json());
+
+  app.get("/health-check", async (_, res) => {
+    return res.json({
+      status: CUSTOM_RESPONSE_STATUS.OK,
+    });
+  });
 
   /*
     `logger` middleware logs the http(s) requests info when the
