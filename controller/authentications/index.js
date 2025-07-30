1267c15f3983c92febbd56c0e87e37a876170ab3
Saurav Sanjay <resourcesatresource@gmail.com>
2025-07-31 00:10:40 +0530

    Return validated data as part of response.

--- controller/authentications/index.js
+++ controller/authentications/index.js
@@ -1,8 +1,16 @@
 const { CUSTOM_RESPONSE_STATUS } = require("../../constants");
 
-const postVerifyAuthTokenHandler = async (_, res) => {
+const postVerifyAuthTokenHandler = async (req, res) => {
+  const { _id: id, name, email, isAdmin, username } = req.user;
   return res.json({
     status: CUSTOM_RESPONSE_STATUS.OK,
+    user: {
+      id,
+      name,
+      email,
+      isAdmin,
+      username,
+    },
   });
 };
 
